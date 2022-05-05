8a8b3bceb70be7ee714362a5355c49b1feaece5d
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-05 15:27:50 +0000

    jsxfile

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const city = "oujda";
-const name = "iliass";
-const age = 27;
-const year = new Date().getFullYear();
+// const city = "oujda";
+// const name = "iliass";
+// const age = 27;
+// const year = new Date().getFullYear();
 
 ReactDOM.render(
   <div>
-    <h2>hi from morocco {city} </h2>
-    <p>
-      this me helo {name} my age {age}
-    </p>
-    <p>
-      Copyright &copy; {year} {name}
-    </p>
+    <h2 className="heading">hi from morocco </h2>
+    <ul>
+      <li>oujda</li>
+      <li>tanger</li>
+      <li>rabat</li>
+    </ul>
+    <p>this me helo my age</p>
+    <p>Copyright &copy;</p>
   </div>,
   document.getElementById("root")
 );
