0b0675d95c3ab853232c26da4f9366017868be88
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-05 13:01:42 +0000

    copy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const city = "oujda";
+const name = "iliass";
+const age = 27;
+const year = new Date().getFullYear();
+
 ReactDOM.render(
   <div>
-    <h1> helo </h1>
-    <ul>
-      my favorete food
-      <li>tacos</li>
-      <li>tacos</li>
-      <li>tacos</li>
-    </ul>
-    <ul>
-      my favorete food
-      <li>piza</li>
-      <li>tacos</li>
-      <li>tacos</li>
-    </ul>
+    <h2>hi from morocco {city} </h2>
+    <p>
+      this me helo {name} my age {age}
+    </p>
+    <p>
+      Copyright &copy; {year} {name}
+    </p>
   </div>,
   document.getElementById("root")
 );
