78f56c346aceb8918e7897928674f2264a084269
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-07 15:36:18 +0000

    jsx file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const city = "oujda";
-// const name = "iliass";
-// const age = 27;
-// const year = new Date().getFullYear();
+const customStyle = {
+  color: "red"
+};
 
 ReactDOM.render(
   <div>
-    <h2 className="heading">hi from morocco </h2>
-    <ul>
-      <li>oujda</li>
-      <li>tanger</li>
-      <li>rabat</li>
-    </ul>
-    <p>this me helo my age</p>
-    <p>Copyright &copy;</p>
+    <h1 style={customStyle}>hi from tanger</h1>{" "}
   </div>,
   document.getElementById("root")
 );
