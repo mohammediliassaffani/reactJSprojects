5aa46d510e6f7300d0f7a7b68b438da9091e4361
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-08 20:44:13 +0100

    exportimport

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import Heading from "./Components/Heading";
+import pi, { year, age, salaire, brth } from "./Math";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Heading />
+      <ul>
+        <li>{pi}</li>
+        <li>{year + 1}</li>
+        <li>{age + 2}</li>
+        <li>{brth()}</li>
+        <li>{salaire()}</li>
+      </ul>
+    </div>
+  );
 }
 export default App;
---
+++ src/Math.js
@@ -0,0 +1,17 @@
+const pi = 3.141516;
+
+const year = 365;
+
+const age = 30;
+
+function brth() {
+  return 92;
+}
+
+function salaire() {
+  return 8000;
+}
+
+export default pi;
+
+export { year, age, brth, salaire };
