b148b8c5a9395bf1fcba217560321d75e40f2bfc
iliass <mohammediliassaffani@gmail.com>
2022-05-18 18:54:58 +0100

    arowFun

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./components/notes";
+
+
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+     {notes.map (( noteItem) => ( 
+  <Note 
+  title = {noteItem.title}
+  content = {noteItem.content}
+  />
+)) }
       <Footer />
     </div>
   );
