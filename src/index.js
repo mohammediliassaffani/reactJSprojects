e207fa6de80862d42513ee96bedd6c6b254cd166
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-18 09:52:29 +0100

    maping

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 var numbers = [3, 56, 2, 48, 5];
 
+// function double(x) {
+//   return x * 2;
+// }
+// const newNumbers = numbers.map(double);
+
+var newNumbers = [];
+
 function double(x) {
-  return x * 2;
+  newNumbers.push(x * 3);
 }
-const newNumbers = numbers.map(double);
+numbers.forEach(double);
 
 console.log(newNumbers);
 
