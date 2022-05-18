4d1168d97cc40a37e303567877c5fe98f108fd51
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-18 10:04:16 +0100

    reduce

--- src/index.js
+++ src/index.js
@@ -4,21 +4,23 @@ var numbers = [3, 56, 2, 48, 5];
 //   return x * 2;
 // }
 // const newNumbers = numbers.map(double);
-
-var newNumbers = [];
-
-function double(x) {
-  newNumbers.push(x * 3);
-}
-numbers.forEach(double);
-
-console.log(newNumbers);
+// console.log(newNumbers);
 
 //Map -Create a new array by doing something with each item in an array.
 
 //Filter - Create a new array by keeping the items that return true.
 
+// const newNumbers = numbers.filter(function (num) {
+//   return num > 10;
+// });
+// console.log(newNumbers);
+
 //Reduce - Accumulate a value by doing something to each item in an array.
+
+var newNumbers = numbers.reduce(function (Accumulate, currentNumber) {
+  return Accumulate + currentNumber;
+});
+console.log(newNumbers);
 
 //Find - find the first item that matches from an array.
 
