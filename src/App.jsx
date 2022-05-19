82caec30160fda3d3114ca41a9027f39f38d4827
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-19 09:55:06 +0100

    userlog

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Login from "./Components/Login";
 
 var isLoggedIn = false;
 
+const currentTime = new Date().getHours();
+
 function App() {
   return (
     <div className="container">
-      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+
+      {currentTime > 12 ? <h1> nod sire t9awad</h1> : null}
     </div>
   );
 }
