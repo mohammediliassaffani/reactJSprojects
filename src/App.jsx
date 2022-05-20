20a0f31fef91f85d689fa8e244241faac07e1e9f
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-20 10:06:59 +0100

    btnClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
+var count = 0;
+
+function incre() {
+  console.log("i got clickt");
+}
+
 function App() {
   return (
     <div className="container">
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={incre}>+</button>
+      <button>-</button>
     </div>
   );
 }
