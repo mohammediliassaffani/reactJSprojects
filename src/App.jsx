a77bda10e417279a268b99f8d6987f9a9f2c349a
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-05-20 11:06:23 +0100

    testHOKS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-
-var count = 0;
-
-function incre() {
-  console.log("i got clickt");
-}
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function incre() {
+    setCount(count + 1);
+  }
+  function incree() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={incre}>+</button>
-      <button>-</button>
+      <button onClick={incree}>-</button>
     </div>
   );
 }
